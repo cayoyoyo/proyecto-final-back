b8dc2528ab04925a2e6aaca2abb43c861929cbe0
Carlos Campagnolo <ccampagnolos@gmail.com>
2023-09-23 11:55:46 +0200

    favorites routes

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User.model"); // Importa el modelo de usuario
+const User = require("../models/User.model");
 
 // Ruta para ver el perfil del usuario
 router.get("/:id", (req, res, next) => {
-  const userId = req.params.id; // Suponiendo que el ID del usuario está disponible en req.user
+  const userId = req.params.id; 
 
   User.findById(userId)
     .then((response) => {
@@ -16,66 +16,81 @@ router.get("/:id", (req, res, next) => {
     });
 });
 
+
 // Ruta para agregar un producto a favoritos del usuario
-router.post("/favoritos/agregar/:id", (req, res) => {
-  const userId = req.user._id; // Suponiendo que el ID del usuario está disponible en req.user
-  const productoId = req.params.id; // ID del producto a agregar a favoritos
-
-  // Encuentra al usuario por su ID y actualiza su lista de favoritos
-  User.findByIdAndUpdate(
-    userId,
-    { $addToSet: { favoritos: productoId } }, // Agrega el producto a la lista de favoritos si no está presente
-    { new: true }, // Devuelve el documento actualizado
-    (err, user) => {
-      if (err) {
-        console.error("Error al agregar el producto a favoritos:", err);
-        return res
-          .status(500)
-          .json({ message: "Error al agregar el producto a favoritos" });
-      }
-
-      if (!user) {
-        return res.status(404).json({ message: "Usuario no encontrado" });
-      }
-
-      // Envía un mensaje de éxito o el usuario actualizado en la respuesta
-      res.json({
-        message: "Producto agregado a favoritos con éxito",
-        usuario: user,
+router.post("/:id/favorites/add", (req, res, next) => {
+    const userId = req.params.id;
+    const productId = req.body.productId; 
+  
+    User.findById(userId)
+      .then((user) => {
+        if (!user) {
+          return res.status(404).json({ message: "User not found" });
+        }
+  
+        if (user.favoriteProducts.includes(productId)) {
+          return res.status(400).json({ message: "The product is already in favorites" });
+        }
+  
+        user.favoriteProducts.push(productId);
+  
+        return user.save();
+      })
+      .then((user) => {
+        const filteredUser = {
+          _id: user._id,
+          email: user.email,
+          name: user.name,
+          favoriteProducts: user.favoriteProducts,
+        };
+  
+        res.status(200).json({ message: "Product added to favorites successfully", user: filteredUser });
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).json({ error });
       });
-    }
-  );
-});
-
+  });
+  
+  
+  
 // Ruta para eliminar un producto de favoritos del usuario
-router.delete("/favoritos/eliminar/:id", (req, res) => {
-  const userId = req.user._id; // Suponiendo que el ID del usuario está disponible en req.user
-  const productoId = req.params.id; // ID del producto a eliminar de favoritos
-
-  // Encuentra al usuario por su ID y actualiza su lista de favoritos
-  User.findByIdAndUpdate(
-    userId,
-    { $pull: { favoritos: productoId } }, // Elimina el producto de la lista de favoritos
-    { new: true }, // Devuelve el documento actualizado
-    (err, user) => {
-      if (err) {
-        console.error("Error al eliminar el producto de favoritos:", err);
-        return res
-          .status(500)
-          .json({ message: "Error al eliminar el producto de favoritos" });
-      }
-
-      if (!user) {
-        return res.status(404).json({ message: "Usuario no encontrado" });
-      }
-
-      // Envía un mensaje de éxito o el usuario actualizado en la respuesta
-      res.json({
-        message: "Producto eliminado de favoritos con éxito",
-        usuario: user,
+router.delete("/:id/favorites/remove", (req, res, next) => {
+    const userId = req.params.id;
+    const productId = req.body.productId; 
+  
+    User.findById(userId)
+      .then((user) => {
+        if (!user) {
+          return res.status(404).json({ message: "User not found" });
+        }
+  
+        const productIndex = user.favoriteProducts.indexOf(productId);
+        if (productIndex === -1) {
+          return res.status(400).json({ message: "The product is not in favorites" });
+        }
+  
+        user.favoriteProducts.splice(productIndex, 1);
+  
+        return user.save();
+      })
+      .then((user) => {
+        const filteredUser = {
+          _id: user._id,
+          email: user.email,
+          name: user.name,
+          favoriteProducts: user.favoriteProducts,
+        };
+  
+        res.status(200).json({ message: "Product removed from favorites successfully", user: filteredUser });
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).json({ error });
       });
-    }
-  );
-});
+  });
+  
+  
+  
 
 module.exports = router;
