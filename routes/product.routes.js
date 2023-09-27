c7687e50bed557a86fa73e480e340286ee7b250a
Carlos Campagnolo <ccampagnolos@gmail.com>
2023-09-27 19:07:26 +0200

    product form con img

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,111 +1,109 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Product = require('../models/Producto.model');
+const fileUploader = require('../config/cloudinaryconfig'); 
+const Product = require("../models/Producto.model");
 
 // router.get('/', (req, res, next) => {
 
 // });
 
-
 //  TODOS LOS PRODUCTOS --------------------------- funciona OK
-router.get('/', (req, res, next) => { //todas las rutas aqui tienen el "/product" antes 
-    Product.find()
-        .then((products) => {
-            console.log("quiero saber que devuelve products " + products)
-            res.status(200).json(products); // Devuelve una respuesta JSON con los productos
-        })
-        .catch((error) => {
-            console.error('Error al recuperar los productos:', error);
-            res.status(500).json({ error: 'Error al recuperar los productos' });
-        });
+router.get("/", (req, res, next) => {
+  //todas las rutas aqui tienen el "/product" antes
+  Product.find()
+    .then((products) => {
+      console.log("quiero saber que devuelve products " + products);
+      res.status(200).json(products); // Devuelve una respuesta JSON con los productos
+    })
+    .catch((error) => {
+      console.error("Error al recuperar los productos:", error);
+      res.status(500).json({ error: "Error al recuperar los productos" });
+    });
 });
 
 // DETALLES DE UN UNICO PRODUCTO --------------------------- funciona OK
-router.get('/:id', (req, res, next) => {
-    const productId = req.params.id;
+router.get("/:id", (req, res, next) => {
+  const productId = req.params.id;
 
-    Product.findById(productId)
-        .then((product) => {
-            if (!product) {
-                return res.status(404).json({ error: 'Producto no encontrado' });
-            }
+  Product.findById(productId)
+    .populate("seller")
+    .then((product) => {
+      if (!product) {
+        return res.status(404).json({ error: "Producto no encontrado" });
+      }
 
-            res.status(200).json(product); // Devuelve un JSON con los detalles del producto 
-        })
-        .catch((error) => {
-            console.error('Error al obtener los detalles del producto:', error);
-            res.status(500).json({ error: 'Error al obtener los detalles del producto' });
-        });
+      res.status(200).json(product); // Devuelve un JSON con los detalles del producto
+    })
+    .catch((error) => {
+      console.error("Error al obtener los detalles del producto:", error);
+      res
+        .status(500)
+        .json({ error: "Error al obtener los detalles del producto" });
+    });
 });
 
 // CREAR UN NUEVO PRODUCTO  --------------------------- funciona OK
-router.post('/add', (req, res, next) => {
-
+router.post('/add', fileUploader.single('product-image'), (req, res) => {
     const { title, description, price, condition, images, seller } = req.body;
-
-    const newProduct = new Product({
-        title,
-        description,
-        price,
-        condition,
-        images,
-    });
-
-    newProduct
-        .save()
-        .then((product) => {
-            res.status(201).json(product);
-        })
-        .catch((error) => {
-            console.error('Error al crear un nuevo producto:', error);
-            res.status(500).json({ error: 'Error al crear un nuevo producto' });
-        });
-
-});
+  
+    // Utiliza el modelo Product para crear y guardar un nuevo producto en la base de datos
+    Product.create({
+      title,
+      description,
+      price,
+      condition,
+      images: [req?.file?.path], // Asígnale la imagen al campo images como un arreglo de una sola imagen
+      seller,
+    })
+      .then((newlyCreatedProductFromDB) => {
+        // Envía una respuesta con el producto recién creado
+        res.json(newlyCreatedProductFromDB);
+      })
+      .catch((error) => {
+        console.error(`Error while creating a new product: ${error}`);
+        res.status(500).json({ error: 'Error while creating a new product' });
+      });
+  });
 
 // EDITAR UN PRODUCTO  --------------------------- funciona OK
-router.get('/:id/edit', (req, res, next) => {
+router.get("/:id/edit", (req, res, next) => {
+  const productId = req.params.id;
+  const { title, description, price, condition, images } = req.body;
 
-    const productId = req.params.id;
-    const { title, description, price, condition, images } = req.body;
+  const updateProduct = {
+    title,
+    description,
+    price,
+    condition,
+    images,
+  };
 
-    const updateProduct = {
-        title,
-        description,
-        price,
-        condition,
-        images,
-    };
+  Product.findByIdAndUpdate(productId, updateProduct, { new: true })
+    .then((updatedProduct) => {
+      if (!updatedProduct) {
+        return res.status(404).json({ error: "Producto no encontrado" });
+      }
 
-    Product.findByIdAndUpdate(productId, updateProduct, { new: true })
-        .then((updatedProduct) => {
-            if (!updatedProduct) {
-                return res.status(404).json({ error: 'Producto no encontrado' });
-            }
-
-            res.status(200).json(updatedProduct);
-        })
-        .catch((error) => {
-            console.error('Error al editar el producto:', error);
-            res.status(500).json({ error: 'Error al editar el producto' });
-        });
-
+      res.status(200).json(updatedProduct);
+    })
+    .catch((error) => {
+      console.error("Error al editar el producto:", error);
+      res.status(500).json({ error: "Error al editar el producto" });
+    });
 });
 
 // DELETAR UN PRODUCTO --------------------------- funciona OK
-router.get('/:id/delete', (req, res, next) => {
+router.get("/:id/delete", (req, res, next) => {
+  const productId = req.params.id;
 
-    const productId = req.params.id;
-
-    Product.findByIdAndRemove(productId)
-        .then(() => {
-            res.status(200).json({ message: 'Producto eliminado con éxito' });
-        })
-        .catch((error) => {
-            console.error('Error al eliminar el producto:', error);
-            res.status(500).json({ error: 'Error al eliminar el producto' });
-        });
-
+  Product.findByIdAndRemove(productId)
+    .then(() => {
+      res.status(200).json({ message: "Producto eliminado con éxito" });
+    })
+    .catch((error) => {
+      console.error("Error al eliminar el producto:", error);
+      res.status(500).json({ error: "Error al eliminar el producto" });
+    });
 });
 
 module.exports = router;
--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -4,10 +4,12 @@ const User = require("../models/User.model");
 
 // Ruta para ver el perfil del usuario
 router.get("/:id", (req, res, next) => {
-  const userId = req.params.id; 
+  const userId = req.params.id;
 
   User.findById(userId)
+    .populate("favoriteProducts productsForSale")
     .then((response) => {
+      console.log(response);
       res.status(200).json(response);
     })
     .catch((error) => {
@@ -16,81 +18,85 @@ router.get("/:id", (req, res, next) => {
     });
 });
 
-
 // Ruta para agregar un producto a favoritos del usuario
-router.post("/:id/favorites/add", (req, res, next) => {
-    const userId = req.params.id;
-    const productId = req.body.productId; 
-  
-    User.findById(userId)
-      .then((user) => {
-        if (!user) {
-          return res.status(404).json({ message: "User not found" });
-        }
-  
-        if (user.favoriteProducts.includes(productId)) {
-          return res.status(400).json({ message: "The product is already in favorites" });
-        }
-  
-        user.favoriteProducts.push(productId);
-  
-        return user.save();
-      })
-      .then((user) => {
-        const filteredUser = {
-          _id: user._id,
-          email: user.email,
-          name: user.name,
-          favoriteProducts: user.favoriteProducts,
-        };
-  
-        res.status(200).json({ message: "Product added to favorites successfully", user: filteredUser });
-      })
-      .catch((error) => {
-        console.error(error);
-        res.status(500).json({ error });
+router.post("/:id/favorites/add/:productId", (req, res, next) => {
+  const userId = req.params.id;
+  const productId = req.params.productId;
+
+  User.findById(userId)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      if (user.favoriteProducts.includes(productId)) {
+        return res
+          .status(400)
+          .json({ message: "The product is already in favorites" });
+      }
+
+      user.favoriteProducts.push(productId);
+
+      return user.save();
+    })
+    .then((user) => {
+      const filteredUser = {
+        _id: user._id,
+        email: user.email,
+        name: user.name,
+        favoriteProducts: user.favoriteProducts,
+      };
+
+      res.status(200).json({
+        message: "Product added to favorites successfully",
+        user: filteredUser,
       });
-  });
-  
-  
-  
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ error });
+    });
+});
+
 // Ruta para eliminar un producto de favoritos del usuario
 router.delete("/:id/favorites/remove", (req, res, next) => {
-    const userId = req.params.id;
-    const productId = req.body.productId; 
-  
-    User.findById(userId)
-      .then((user) => {
-        if (!user) {
-          return res.status(404).json({ message: "User not found" });
-        }
-  
-        const productIndex = user.favoriteProducts.indexOf(productId);
-        if (productIndex === -1) {
-          return res.status(400).json({ message: "The product is not in favorites" });
-        }
-  
-        user.favoriteProducts.splice(productIndex, 1);
-  
-        return user.save();
-      })
-      .then((user) => {
-        const filteredUser = {
-          _id: user._id,
-          email: user.email,
-          name: user.name,
-          favoriteProducts: user.favoriteProducts,
-        };
-  
-        res.status(200).json({ message: "Product removed from favorites successfully", user: filteredUser });
-      })
-      .catch((error) => {
-        console.error(error);
-        res.status(500).json({ error });
+  const userId = req.params.id;
+  const productId = req.body.productId;
+
+  User.findById(userId)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      const productIndex = user.favoriteProducts.indexOf(productId);
+      if (productIndex === -1) {
+        return res
+          .status(400)
+          .json({ message: "The product is not in favorites" });
+      }
+
+      user.favoriteProducts.splice(productIndex, 1);
+
+      return user.save();
+    })
+    .then((user) => {
+      const filteredUser = {
+        _id: user._id,
+        email: user.email,
+        name: user.name,
+        favoriteProducts: user.favoriteProducts,
+      };
+
+      res.status(200).json({
+        message: "Product removed from favorites successfully",
+        user: filteredUser,
       });
-  });
-  
-  
-  
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ error });
+    });
+});
 
 module.exports = router;
